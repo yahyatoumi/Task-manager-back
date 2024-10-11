ef0fcaedbfdd655a7a9353ea7cb7657588d5028d
yahyatoumi <yahyatoumi555@gmail.com>
2024-10-11 09:44:56 +0100

    loading problem fix

--- sideproject/asgi.py
+++ sideproject/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 from django_channels_jwt.middleware import JwtAuthMiddlewareStack
@@ -21,7 +22,6 @@ def get_websocket_urlpatterns():
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sideproject.settings')
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
