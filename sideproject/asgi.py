4b85b539fdc3769a0d1623ce1be3de6e42b4f97c
yahyatoumi <yahyatoumi555@gmail.com>
2024-10-11 09:41:47 +0100

    loading problem fix

--- sideproject/asgi.py
+++ sideproject/asgi.py
@@ -8,16 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from taskmanager.consumers import TextRoomConsumer
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 from django_channels_jwt.middleware import JwtAuthMiddlewareStack
 
-websocket_urlpatterns = [
-    re_path(r'^ws/board/(?P<board_id>\d+)/$', TextRoomConsumer.as_asgi()),
-]
+def get_websocket_urlpatterns():
+    from taskmanager.consumers import TextRoomConsumer
+    return [
+        re_path(r'^ws/board/(?P<board_id>\d+)/$', TextRoomConsumer.as_asgi()),
+    ]
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sideproject.settings')
 
@@ -27,7 +27,7 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
     'websocket':JwtAuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns,
+            get_websocket_urlpatterns(),
         )
     )
     ,
